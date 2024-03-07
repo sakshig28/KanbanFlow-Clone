4d5eb2b86ade653c5b3df4e9f52832700a05065d
Paulo Xu <pauloxu@PauloAir.local>
2024-03-07 17:11:24 -0500

    set up firebase, added auth

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Board from './components/Board';
 import './App.css';
+import { Auth } from './components/auth';
 
 function App() {
   return (
     <div className="App">
+      <Auth/>
       <header className="App-header">
         <h1 className="App-title">Trello Clone</h1>
       </header>
